a472edc1ca4c1c935fe9eb12d68cf2d3ef1d1282
Daniel Bueno <Dbueno3@buffalo.edu>
2023-03-12 17:34:21 -0400

    Rendering Homepage #54

---
+++ backend/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My React App</title>
+</head>
+<body>
+<div id="root"></div>
+<script src="/Users/dbueno3/Downloads/project_s23-welovehertz/frontend/src/App.js"></script>
+</body>
+</html>
--- frontend/src/pages/loginbar.jsx
+++ frontend/src/pages/loginbar.jsx
@@ -24,7 +24,7 @@ export default function Login (page) {
                         <input value={email} onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Email" name="email" />
                         <label for="password">Password</label>
                         <input value={password} onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Password" name="password" />
-                        <button><Link to="/">LOGIN</Link></button>
+                        <button><Link to="">LOGIN</Link></button>
                     </form>
                     <button className="register-button" ><Link to="/register">Don't Have An Account? Click Here</Link></button>
                 </div>
