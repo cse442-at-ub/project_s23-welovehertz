3e0811ed5f8a256d1f57354c14b64ac93c67c574
Daniel Bueno <Dbueno3@buffalo.edu>
2023-03-18 15:04:58 -0400

    Added updates on the PHP file for #54

--- backend/hompage.php
+++ backend/hompage.php
@@ -9,6 +9,8 @@ $html = str_replace('<div id="root"></div>', '<div id="root">' . renderReactApp(
 echo $html;
 
 // Render the React app's root element
+
+// forgot to add the task # lol 
 function renderReactApp() {
 
     $loop = \React\EventLoop\Factory::create();
@@ -20,4 +22,5 @@ function renderReactApp() {
     return $reactphpRenderer->render($component);
 }
 ?>
+
 
