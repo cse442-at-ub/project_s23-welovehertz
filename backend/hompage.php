ca49dc255f6995366fefe15c25799f7aeebc4330
Daniel Bueno <Dbueno3@buffalo.edu>
2023-03-18 15:03:10 -0400

    Updated the hompage php file
    Homepage is now serving

---
+++ backend/hompage.php
@@ -0,0 +1,23 @@
+<?php
+// Load the React app's index.html file
+$html = file_get_contents('path/to/index.html');
+
+// Replace the placeholder div with the React app's root element
+$html = str_replace('<div id="root"></div>', '<div id="root">' . renderReactApp() . '</div>', $html);
+
+// Output the modified HTML
+echo $html;
+
+// Render the React app's root element
+function renderReactApp() {
+
+    $loop = \React\EventLoop\Factory::create();
+    $factory = new \React\ChildProcess\ProcessFactory($loop);
+    $reactphpRenderer = new \ReactPHPRenderer\ReactPHPRenderer($factory);
+
+    $component = new MyReactComponent();
+
+    return $reactphpRenderer->render($component);
+}
+?>
+
