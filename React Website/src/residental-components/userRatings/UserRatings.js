4e35c881b0dd8529a22ca23a22c6e727eedbfcc6
ksy990719 <ksy990719@hotmail.com>
2023-04-23 16:48:19 -0400

    Updated rating form #96

--- React Website/src/residental-components/userRatings/UserRatings.js
+++ React Website/src/residental-components/userRatings/UserRatings.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import { useParams } from 'react-router-dom'
+
 import pfp from "./images/UserRatings-profile-icon.png"
 import likeButton from "./images/UserRatings-like-button.png"
 import dislikeButton from "./images/UserRatings-dislike-button.png"
+import Axios from "axios"
 import "./userRatings.css"
 import "./popup.css"
 
 export default function UserRatings() {
+    const [showIncompleteFormError, setShowIncompleteFormError] = useState(false);
+    const [formIncomplete, setFormIncomplete] = useState(true);
     const [comment, setComment] = useState("");
     const [comments, setComments] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
@@ -15,45 +20,68 @@ export default function UserRatings() {
     const [locationRating, setLocationRating] = useState(0);
     const [interiorRating, setInteriorRating] = useState(0);
     const [safetyRating, setSafetyRating] = useState(0);
+    let { pageid } = useParams();
+    const handleFormChange = () => {
+        if (
+            cleanlinessRating !== 0 &&
+            priceRating !== 0 &&
+            communicationRating !== 0 &&
+            locationRating !== 0 &&
+            interiorRating !== 0 &&
+            safetyRating !== 0 &&
+            comment.trim() !== ""
+        ) {
+            setFormIncomplete(false);
+        } else {
+            setFormIncomplete(true);
+        }
+    };
 
-    const handleCommentChange = (event) => {
-        setComment(event.target.value);
-    }
 
-    const handleCommentSubmit = () => {
-        setComments([...comments, comment]);
-        setComment("");
-    }
-
-    const handleCleanlinessRatingChange = (event) => {
-        setCleanlinessRating(event.target.value);
-    }
-
-    const handlePriceRatingChange = (event) => {
-        setPriceRating(event.target.value);
-    }
-
-    const handleCommunicationRatingChange = (event) => {
-        setCommunicationRating(event.target.value);
-    }
-
-    const handleLocationRatingChange = (event) => {
-        setLocationRating(event.target.value);
-    }
-
-    const handleInteriorRatingChange = (event) => {
-        setInteriorRating(event.target.value);
-    }
-
-    const handleSafetyRatingChange = (event) => {
-        setSafetyRating(event.target.value);
-    }
-
-    const handlePopupSubmit = () => {
-        // handle submission of ratings
-        setShowPopup(false);
-    }
-
+    const handlePopupSubmit = (event) => {
+        event.preventDefault();
+        let cookie = document.cookie
+        let parsedCookie = cookie.substring(cookie.indexOf("currentUserCookie") + 18)
+        if (!(parsedCookie.indexOf(";") == -1)) {
+            parsedCookie = parsedCookie.substring(0, parsedCookie.indexOf(";"))
+        }
+        const data = {
+            cleanlinessRating: cleanlinessRating,
+            priceRating: priceRating,
+            communicationRating: communicationRating,
+            locationRating: locationRating,
+            interiorRating: interiorRating,
+            safetyRating: safetyRating,
+            comment: comment,
+        };
+    
+        if (
+            cleanlinessRating === 0 ||
+            priceRating === 0 ||
+            communicationRating === 0 ||
+            locationRating === 0 ||
+            interiorRating === 0 ||
+            safetyRating === 0 ||
+            comment.trim() === ""
+        ) {
+            setShowIncompleteFormError(true);
+            return;
+        }
+    
+        Axios.post('https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442h/backend/profile.php', {
+            data, id: parsedCookie, pageid
+        })
+            .then((response) => {
+                console.log(response);
+                setShowPopup(false);
+                // do something else after submission
+            })
+            .catch((error) => {
+                console.log(error);
+                // handle errors
+            });
+    };
+    
     return (
         <div className="userRatings">
             <div className="userRatings-row">
@@ -111,7 +139,7 @@ export default function UserRatings() {
                         <h3>Leave a Comment and Rating</h3>
                         <div className="popupForm">
                             <label>Cleanliness:</label>
-                            <select value={cleanlinessRating} onChange={handleCleanlinessRatingChange}>
+                            <select value={cleanlinessRating} onChange={(event) => { setCleanlinessRating(event.target.value); handleFormChange(); }}>
                                 <option value={0}>Select rating...</option>
                                 <option value={1}>1</option>
                                 <option value={2}>2</option>
@@ -121,7 +149,7 @@ export default function UserRatings() {
                             </select>
 
                             <label>Price:</label>
-                            <select value={priceRating} onChange={handlePriceRatingChange}>
+                            <select value={priceRating} onChange={(event) => { setPriceRating(event.target.value); handleFormChange(); }}>
                                 <option value={0}>Select rating...</option>
                                 <option value={1}>1</option>
                                 <option value={2}>2</option>
@@ -131,7 +159,7 @@ export default function UserRatings() {
                             </select>
 
                             <label>Communication:</label>
-                            <select value={communicationRating} onChange={handleCommunicationRatingChange}>
+                            <select value={communicationRating} onChange={(event) => { setCommunicationRating(event.target.value); handleFormChange(); }}>
                                 <option value={0}>Select rating...</option>
                                 <option value={1}>1</option>
                                 <option value={2}>2</option>
@@ -141,7 +169,7 @@ export default function UserRatings() {
                             </select>
 
                             <label>Location:</label>
-                            <select value={locationRating} onChange={handleLocationRatingChange}>
+                            <select value={locationRating} onChange={(event) => { setLocationRating(event.target.value); handleFormChange(); }}>
                                 <option value={0}>Select rating...</option>
                                 <option value={1}>1</option>
                                 <option value={2}>2</option>
@@ -151,7 +179,7 @@ export default function UserRatings() {
                             </select>
 
                             <label>Interior:</label>
-                            <select value={interiorRating} onChange={handleInteriorRatingChange}>
+                            <select value={interiorRating} onChange={(event) => { setInteriorRating(event.target.value); handleFormChange(); }}>
                                 <option value={0}>Select rating...</option>
                                 <option value={1}>1</option>
                                 <option value={2}>2</option>
@@ -160,7 +188,7 @@ export default function UserRatings() {
                                 <option value={5}>5</option>
                             </select>
                             <label>Safety:</label>
-                            <select value={safetyRating} onChange={handleSafetyRatingChange}>
+                            <select value={safetyRating} onChange={(event) => { setSafetyRating(event.target.value); handleFormChange(); }}>
                                 <option value={0}>Select rating...</option>
                                 <option value={1}>1</option>
                                 <option value={2}>2</option>
@@ -170,12 +198,15 @@ export default function UserRatings() {
                             </select>
 
                             <label>Comment:</label>
-                            <textarea value={comment} onChange={handleCommentChange}></textarea>
+                            <textarea value={comment} onChange={(event) => { setComment(event.target.value); handleFormChange(); }}></textarea>
 
-                            <button onClick={handlePopupSubmit}>Submit</button>
+            <button onClick={handlePopupSubmit}>Submit</button>
                             <button className="popupCancelButton" onClick={() => setShowPopup(false)}>Cancel</button>
+                            {showIncompleteFormError && <p3 >Please complete the form.</p3>}
+
                         </div>
                     </div>
+
                 )}
 
                 {comments.map((comment, index) => (
